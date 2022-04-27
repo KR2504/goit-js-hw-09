56d03e1b0d3c260a245669750fee343f007200d9
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-04-27 15:28:10 +0300

    task-1-js-hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+let changeColor = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onBtnClickStartToChangeColor);
+refs.stopBtn.addEventListener('click', onBtnClickStopChangeColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnClickStartToChangeColor() {
+    changeColor = setInterval(() => {
+        const backgroundColor = getRandomHexColor();
+        refs.body.style.backgroundColor = backgroundColor;
+    }, 1000)
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onBtnClickStopChangeColor() {
+    clearInterval(changeColor);
+
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
