c85ebd858dfb41497f07b99a2537a310f91e8af5
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-05-01 20:33:46 +0300

    task-3-js-hw-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const {
+        elements: { delay, step, amount }
+    } = e.currentTarget;
+
+    let position = 1;
+    let delayValue = Number(delay.value);
+    const delayStep = Number(step.value);
+    const number = Number(amount.value);
+
+    for (let i = 1; i <= number; i += 1) {
+        createPromise(position, delayValue)
+            .then(({ position, delay }) => {
+                Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+
+        position += 1;
+        delayValue += delayStep;
+    }
+
+    e.currentTarget.reset();
 }
+
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay })
+            } else {
+                reject({ position, delay })
+            }
+        }, delay)
+    })
+}
+
+Notify.init({
+    width: '280px',
+    position: 'right-top',
+    distance: '30px',
+    borderRadius: '10px',
+    timeout: 4000,
+    cssAnimationStyle: 'from-right',
+})
